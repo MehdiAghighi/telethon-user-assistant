07752515ad020ecfc611a39f294d0ce6b3a0a5bc
MehdiAghighi <mamaliosezar@gmail.com>
2020-04-20 18:24:55 +0430

    set the path of config and session file relativly

--- Vendor/Client.py
+++ Vendor/Client.py
@@ -2,17 +2,21 @@ from telethon import TelegramClient, events, types, functions, sync
 import socks
 import configparser
 import logging
+from os import path, getcwd
+
+# Set the location of the script
+script_dir = path.abspath(path.join(path.dirname(__file__), '..'))
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 # Loading Environment varriables
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(path.join(script_dir, "config.ini"))
 
 
 client = TelegramClient(
-    'Assist',
+    path.join(script_dir, "Assist"),
     config['ACCOUNT']['API_ID'],
     config['ACCOUNT']['API_HASH'],
     proxy=(socks.SOCKS5, '127.0.0.1',
